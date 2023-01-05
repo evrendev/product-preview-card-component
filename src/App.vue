33d220d6d17127d55bab302a78e955fc89f2ca28
Evren Yeniev <mail@evren.dev>
2023-01-05 20:45:29 +0300

    Hello World!

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+  name: "App",
+  setup() {
+    return {};
+  },
+};
+</script>
+
+<template>
+  <div>App</div>
+</template>
+
+<style lang="scss">
+@import "@/assets/styles/app.scss";
+</style>
